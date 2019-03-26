32ba8c938d6a6e670cc954219fb402b2d375141a
mehmetcantula <mehmetcantula@gmail.com>
2019-03-27 00:38:45 +0300

    Save metodu duzenlendi. Data Loader eklendi.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -13,4 +13,5 @@ public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
 
     List<Owner> findAllByLastNameLike(String lastName);
- }
+
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,14 +1,13 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
+import java.util.*;
 
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -18,9 +17,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
-
+    T save(T object){
+        try{
+            if(object != null){
+                if(object.getId() == null){
+                    object.setId(getNextId());
+                }
+            }
+            map.put(object.getId(), object);
+        }catch (Exception e) {
+            throw new RuntimeException("Object Cannot be null");
+        }
         return object;
     }
 
@@ -31,4 +38,15 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
+    }
+
 }
