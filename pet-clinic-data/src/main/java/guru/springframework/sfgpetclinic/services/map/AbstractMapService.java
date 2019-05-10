a362a9c1ad3d27f59c6fb70b721c004fa05c01b8
mehmetcantula <mehmetcantula@gmail.com>
2019-05-10 16:28:55 +0300

    dosyalar guncellendi..

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,14 +1,17 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
+import java.util.*;
+//import java.util.HashMap;
+//import java.util.HashSet;
+//import java.util.Map;
+//import java.util.Set;
 
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -18,9 +21,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
-
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -28,7 +37,18 @@ public abstract class AbstractMapService<T, ID> {
         map.remove(id);
     }
 
-    void delete(T object){
-        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    void delete(T object) { map.entrySet().removeIf(entry -> entry.getValue().equals(object)); }
+
+    private Long getNextId(){
+
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
